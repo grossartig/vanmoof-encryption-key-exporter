6cf19eaff33a73c4d3e2a54e1175943106999c33
Justus Dietrich <git@justus-d.de>
2023-07-03 18:47:16 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -98,9 +98,8 @@ export default function Home() {
 				<meta name="description" content="VanMoof Encryption Key Exporter" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				{/* <link rel="icon" href="/favicon.ico" /> */}
-				{/* <script defer data-domain="vanoof.grossartig.io" src="https://plausible.grossartig.io/js/script.js"></script> */}
-				<Script defer data-domain="vanoof.grossartig.io" src="https://plausible.grossartig.io/js/script.js" />
 			</Head>
+			<Script defer data-domain="vanoof.grossartig.io" src="https://plausible.grossartig.io/js/script.js" />
 			<ThemeProvider theme={theme}>
 				<CssBaseline />
 				<main>

3696cd416c1e5efaf7a39b27c6c2c79bbbab60bc
Justus Dietrich <git@justus-d.de>
2023-07-13 23:38:49 +0200

    Horizontal line

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,16 +2,19 @@ import Link from '@mui/material/Link';
 
 export default function Footer() {
 	return (
-		<footer style={{ textAlign: "center", marginTop: "60px" }}>
-			<Link href="https://grossartig.io/keyexport/legalnotice">Legal Notice (Impressum)</Link>&nbsp;&nbsp;-&nbsp;&nbsp;<Link href="https://grossartig.io/keyexport/privacy">Privacy Policy</Link>
-			<br /><br />
-			This project is <Link href="https://github.com/grossartig/vanmoof-encryption-key-exporter" target="_blank">open source</Link>.
-			<br /><br />
-			Made with &#x1F499; in Germany.<br />
-			Coding: <Link href="https://justus-d.de" target="_blank">Justus Dietrich</Link><br />
-			Concept: <Link href="https://angelmann.net" target="_blank">Marius Angelmann</Link>
-			<br /><br />
-			&copy; grossartig.io, 2023
+		<footer style={{ textAlign: "center", marginTop: "64px" }}>
+			<hr />
+			<div style={{ marginTop: "24px" }}>
+				<Link href="https://grossartig.io/keyexport/legalnotice">Legal Notice (Impressum)</Link>&nbsp;&nbsp;-&nbsp;&nbsp;<Link href="https://grossartig.io/keyexport/privacy">Privacy Policy</Link>
+				<br /><br />
+				This project is <Link href="https://github.com/grossartig/vanmoof-encryption-key-exporter" target="_blank">open source</Link>.
+				<br /><br />
+				Made with &#x1F499; in Germany.<br />
+				Coding: <Link href="https://justus-d.de" target="_blank">Justus Dietrich</Link><br />
+				Concept: <Link href="https://angelmann.net" target="_blank">Marius Angelmann</Link>
+				<br /><br />
+				&copy; grossartig.io, 2023
+			</div>
 		</footer>
 	)
 }

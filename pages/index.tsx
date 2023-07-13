b76805646d9a648c1aaf932e7c3851e7a50ebbef
Justus Dietrich <git@justus-d.de>
2023-07-13 12:34:34 +0200

    Updated Footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -251,9 +251,13 @@ export default function Home() {
 							</p>
 						</div>}
 						<footer style={{ textAlign: "center", marginTop: "60px" }}>
-							<Link href="https://grossartig.io/keyexport/legalnotice">Legal Notice (Impressum)</Link>&nbsp;-&nbsp;<Link href="https://grossartig.io/keyexport/privacy">Privacy Policy</Link>
+							<Link href="https://grossartig.io/keyexport/legalnotice">Legal Notice (Impressum)</Link>&nbsp;&nbsp;-&nbsp;&nbsp;<Link href="https://grossartig.io/keyexport/privacy">Privacy Policy</Link>
 							<br /><br />
 							This project is <Link href="https://github.com/grossartig/vanmoof-encryption-key-exporter" target="_blank">open source</Link>.
+							<br /><br />
+							Made with &#x1F499; in Germany.<br />
+							Coding: <Link href="https://justus-d.de" target="_blank">Justus Dietrich</Link><br />
+							Concept: <Link href="https://angelmann.net" target="_blank">Marius Angelmann</Link>
 							<br /><br />
 							&copy; grossartig.io, 2023
 						</footer>
